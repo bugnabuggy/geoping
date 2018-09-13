2bbedf06f9bdb94a79a5b964b482eb480d78c171
Artem Adamov <artem_98adamov_98@mail.ru>
2018-09-13 15:06:43 +0600

    ready to rewiew

--- src/GeoPing.FrontEnd/src/pages/loginPage.tsx
+++ src/GeoPing.FrontEnd/src/pages/loginPage.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import { ILoginPageProps } from '../componentProps/loginPageProps';
+import  registerComponent from "../components/forms/register";
 
 export default class LoginPage extends React.Component<ILoginPageProps, any> {
   render() {
     const form: any = {
       '/resetpassword': 'Reset Password',
-      '/register': 'Register',
+      '/register': registerComponent,
       '/login': 'Login',
     };
-
+    const Component: any = form[this.props.location.pathname];
     return (
       <React.Fragment>
-        {form[this.props.location.pathname]}
+        <Component />
       </React.Fragment>
     );
   }
