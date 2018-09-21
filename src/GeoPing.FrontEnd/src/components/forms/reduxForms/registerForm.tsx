a35aa88e5f5f3247d82fd6e7f5b1eaf83c8febf3
Artem Adamov <artem_98adamov_98@mail.ru>
2018-09-21 12:38:30 +0600

    last fixes

--- src/GeoPing.FrontEnd/src/components/forms/reduxForms/registerForm.tsx
+++ src/GeoPing.FrontEnd/src/components/forms/reduxForms/registerForm.tsx
@@ -1,79 +1,93 @@
 import * as React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import {validate} from '../../../validations/registerFormValidate'
+import { validate } from '../../../validations/registerFormValidate';
 import { FormControl, FormGroup, ControlLabel, Checkbox,  Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import  * as ReactTooltip  from 'react-tooltip';
 import ReCAPTCHA from 'react-google-recaptcha';
+import { IconLookup } from '@fortawesome/fontawesome-svg-core';
+
+const checkCircleIcon: IconLookup = { prefix: 'far', iconName: 'check-circle' };
+const timesCircleIcon: IconLookup = { prefix: 'far', iconName: 'times-circle' };
 
 const renderInput = ( props: any) => {
   return (
     <FormGroup>
       <ControlLabel>{props.labelName}</ControlLabel>{' '}
-      <FormControl
-        {...props.input}
-        type = 'text'
-        placeholder = {props.placeholder}
-        data-tip = {props.meta.error}
-      />
-      <ReactTooltip  disable={!(props.meta.touched && (props.meta.error ))} delayHide={500} />
-      {!props.meta.error &&
-      <div className="isValid-check" >
-          <FontAwesomeIcon icon="check" />
-      </div>}
-
+      <div className="form-input-container">
+        <FormControl
+          {...props.input}
+          type={(props.labelName === 'Password') || (props.labelName === 'Confirm Password') ? 'password' : 'text'}
+          placeholder={props.placeholder}
+          data-tip={props.meta.error}
+        />
+        <div className="form-icon-container">
+          {props.meta.touched ?
+            props.meta.error ?
+              <FontAwesomeIcon icon={timesCircleIcon} className="form-icon-times"/>
+              :
+              <FontAwesomeIcon icon={checkCircleIcon} className="form-icon-check"/>
+            :
+            null
+          }
+          {props.meta.touched &&
+          !props.meta.active &&
+          props.meta.error &&
+          <div className="tooltip_form">{props.meta.error}</div>}
+        </div>
+      </div>
     </FormGroup>
   );
 };
 const termsOfService = () => {
   return (
-       <Checkbox readOnly>
+       <Checkbox readOnly={true}>
          Aggre to the terms of service
        </Checkbox>
   );
 };
 
 function onChange(value: any) {
-
+return true;
 }
 
 function registerForms(props: any): any {
   const {handleSubmit}  = props;
   return(
-    <form className = 'register-form'>
+    <form className="register-form">
       <Field
-        component = {renderInput}
-        name = 'login'
-        labelName = 'Login'
+        component={renderInput}
+        name="login"
+        labelName="Login"
       />
       <Field
-        component = {renderInput}
-        name = 'email'
-        labelName = 'Email'
+        component={renderInput}
+        name="email"
+        labelName="Email"
       />
       <Field
-        component = {renderInput}
-        name = 'password'
-        labelName = 'Password'
+        component={renderInput}
+        name="password"
+        labelName="Password"
       />
       <Field
-        component = {renderInput}
-        name = 'confirmPassword'
-        labelName = 'Confirm Password'
+        component={renderInput}
+        name="confirmPassword"
+        labelName="Confirm Password"
       />
       <Field
-        component = {termsOfService}
-        name ='termsOfService'
+        component={termsOfService}
+        name="termsOfService"
       />
       <ReCAPTCHA
         onChange={onChange}
+        z-index="1"
         sitekey="6LcJA3AUAAAAAPnLVNeX96LdBvtBHiFx5JQlG9oS"
       />
       <Button
-        bsStyle = "primary"
-        type = "submit"
-        className='register-btn'
-        onClick = {handleSubmit}
+        bsStyle="primary"
+        type="submit"
+        className="register-btn"
+        onClick={handleSubmit}
       >
         Register
       </Button>
