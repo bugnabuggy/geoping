e042a0c0811db62c267877024610f3017478b63e
Ivaschenko Igor <ivaschenko.i.y@gmail.com>
2018-09-24 13:13:08 +0600

    Add pre-commit to package.json/GP-39

--- src/GeoPing.FrontEnd/src/components/forms/reduxForms/registerForm.tsx
+++ src/GeoPing.FrontEnd/src/components/forms/reduxForms/registerForm.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import {validate} from '../../../validations/registerFormValidate'
-import { FormControl, FormGroup, ControlLabel, Checkbox,  Button } from 'react-bootstrap';
+import { validate } from '../../../validations/registerFormValidate';
+import { Button, Checkbox, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import  * as ReactTooltip  from 'react-tooltip';
+// import  * as ReactTooltip  from 'react-tooltip';
 import ReCAPTCHA from 'react-google-recaptcha';
 
-const renderInput = ( props: any) => {
+const renderInput = ( props: any ) => {
   return (
     <FormGroup>
       <ControlLabel>{props.labelName}</ControlLabel>{' '}
       <FormControl
         {...props.input}
-        type = 'text'
-        placeholder = {props.placeholder}
-        data-tip = {props.meta.error}
+        type="text"
+        placeholder={props.placeholder}
+        data-tip={props.meta.error}
       />
-      <ReactTooltip  disable={!(props.meta.touched && (props.meta.error ))} delayHide={500} />
+      {/*<ReactTooltip  disable={!(props.meta.touched && (props.meta.error ))} delayHide={500} />*/}
       {!props.meta.error &&
-      <div className="isValid-check" >
-          <FontAwesomeIcon icon="check" />
+      <div className="isValid-check">
+        <FontAwesomeIcon icon="check"/>
       </div>}
 
     </FormGroup>
@@ -27,62 +27,62 @@ const renderInput = ( props: any) => {
 };
 const termsOfService = () => {
   return (
-       <Checkbox readOnly>
-         Aggre to the terms of service
-       </Checkbox>
+    <Checkbox readOnly={true}>
+      Aggre to the terms of service
+    </Checkbox>
   );
 };
 
-function onChange(value: any) {
-
+function onChange( value: any ) {
+  const a: any = 5;
 }
 
-function registerForms(props: any): any {
-  const {handleSubmit}  = props;
-  return(
-    <form className = 'register-form'>
+function registerForms( props: any ): any {
+  const { handleSubmit } = props;
+  return (
+    <form className="register-form">
       <Field
-        component = {renderInput}
-        name = 'login'
-        labelName = 'Login'
+        component={renderInput}
+        name="login"
+        labelName="Login"
       />
       <Field
-        component = {renderInput}
-        name = 'email'
-        labelName = 'Email'
+        component={renderInput}
+        name="email"
+        labelName="Email"
       />
       <Field
-        component = {renderInput}
-        name = 'password'
-        labelName = 'Password'
+        component={renderInput}
+        name="password"
+        labelName="Password"
       />
       <Field
-        component = {renderInput}
-        name = 'confirmPassword'
-        labelName = 'Confirm Password'
+        component={renderInput}
+        name="confirmPassword"
+        labelName="Confirm Password"
       />
       <Field
-        component = {termsOfService}
-        name ='termsOfService'
+        component={termsOfService}
+        name="termsOfService"
       />
       <ReCAPTCHA
         onChange={onChange}
         sitekey="6LcJA3AUAAAAAPnLVNeX96LdBvtBHiFx5JQlG9oS"
       />
       <Button
-        bsStyle = "primary"
-        type = "submit"
-        className='register-btn'
-        onClick = {handleSubmit}
+        bsStyle="primary"
+        type="submit"
+        className="register-btn"
+        onClick={handleSubmit}
       >
         Register
       </Button>
     </form>
   );
 }
-const registerReduxForm: any = reduxForm({
-  form: 'register',
-  validate
 
-})((registerForms));
+const registerReduxForm: any = reduxForm ( {
+  form: 'register',
+  validate,
+} ) ( ( registerForms ) );
 export default registerReduxForm;
