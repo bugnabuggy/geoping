f65b0c9e12d2772dd53a65452e3d12b8eea7d607
ShefardPT <shefard55r@yandex.ru>
2018-10-25 18:41:45 +0600

    - Missed migration in last commit, so here it is

---
+++ src/GeoPing.Infrastructure/Data/Migrations/20181025123921_GPUserGotAvatarField.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeoPing.Infrastructure.Data.Migrations
+{
+    public partial class GPUserGotAvatarField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Avatar",
+                table: "GPUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "GPUsers");
+        }
+    }
+}
