d0cfb64bf0e7f49e998a7a37458c03680fd1a3c8
ShefardPT <shefard55r@yandex.ru>
2018-11-02 15:56:35 +0600

    - Fixed format of testdata

--- tests/GeoPing.TestData/Data/TestChecksIn.cs
+++ tests/GeoPing.TestData/Data/TestChecksIn.cs
@@ -12,8 +12,8 @@ namespace GeoPing.TestData.Data
             new CheckIn
             {
                 Id = Guid.Parse("10000000-0000-0000-0000-000000000001"),
-                Latitude = "55",
-                Longitude = "75",
+                Latitude = 55,
+                Longitude = 75,
                 Distance = 0,
                 Date = DateTime.UtcNow,
                 UserId = Guid.Parse("10000000-0000-0000-0000-000000000001"),
@@ -22,8 +22,8 @@ namespace GeoPing.TestData.Data
             new CheckIn
             {
                 Id = Guid.Parse("10000000-0000-0000-0000-000000000002"),
-                Latitude = "55",
-                Longitude = "75",
+                Latitude = 55,
+                Longitude = 75,
                 Distance = 0,
                 Date = DateTime.UtcNow,
                 UserId = Guid.Parse("10000000-0000-0000-0000-000000000002"),
@@ -32,8 +32,8 @@ namespace GeoPing.TestData.Data
             new CheckIn
             {
                 Id = Guid.Parse("10000000-0000-0000-0000-000000000003"),
-                Latitude = "55",
-                Longitude = "75",
+                Latitude = 55,
+                Longitude = 75,
                 Distance = 0,
                 Date = DateTime.UtcNow,
                 UserId = Guid.Parse("10000000-0000-0000-0000-000000000001"),
@@ -42,8 +42,8 @@ namespace GeoPing.TestData.Data
             new CheckIn
             {
                 Id = Guid.Parse("10000000-0000-0000-0000-000000000004"),
-                Latitude = "55",
-                Longitude = "75",
+                Latitude = 55,
+                Longitude = 75,
                 Distance = 0,
                 Date = DateTime.UtcNow,
                 UserId = Guid.Parse("10000000-0000-0000-0000-000000000002"),
