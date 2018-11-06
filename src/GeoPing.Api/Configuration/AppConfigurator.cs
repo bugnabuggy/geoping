2067b12e69ed86ce6d618694ec3b2690b62f89b4
ShefardPT <shefard55r@yandex.ru>
2018-11-06 18:48:12 +0600

    - Avatar field type of GeoPingUser changed to string, - Related migration, - Corrected methods to get full profile data, short profile data and to edit profile data

--- src/GeoPing.Api/Configuration/AppConfigurator.cs
+++ src/GeoPing.Api/Configuration/AppConfigurator.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Geoping.Services;
+using Geoping.Services.Configuration;
 
 namespace GeoPing.Api.Configuration
 {
@@ -97,7 +98,8 @@ namespace GeoPing.Api.Configuration
                         Email = user.Email,
                         Login = user.UserName,
                         AccountType = "premium",
-                        IsActivated = true
+                        IsActivated = true,
+                        Avatar = DefaultUserSettings.AvatarImage
                     });
                     ctx.SaveChanges();
                 }
--- src/GeoPing.Core/Models/Entities/GeoPingUser.cs
+++ src/GeoPing.Core/Models/Entities/GeoPingUser.cs
@@ -17,6 +17,6 @@ namespace GeoPing.Core.Entities
         public DateTime LastPaid { get; set; }
         public bool IsActivated { get; set; }
         public string IdentityId { get; set; }
-        public byte[] Avatar { get; set; }
+        public string Avatar { get; set; }
     }
 }
