5f1cf56a116194b66faa97c212c259f6543e837a
ShefardPT <shefard55r@yandex.ru>
2018-11-08 18:23:54 +0600

    some fields of CheckInStatsDTO are nullable now

--- src/GeoPing.Core/Models/DTO/CheckInStatsDTO.cs
+++ src/GeoPing.Core/Models/DTO/CheckInStatsDTO.cs
@@ -24,12 +24,12 @@ namespace GeoPing.Core.Models.DTO
 
     public class CheckInStatCheckDTO
     {
-        public Guid UserId { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public double Distance { get; set; }
-        public DateTime Date { get; set; }
-        public Guid DeviceId { get; set; }
+        public Guid? UserId { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public double? Distance { get; set; }
+        public DateTime? Date { get; set; }
+        public Guid? DeviceId { get; set; }
         public string Ip { get; set; }
         public string UserAgent { get; set; }
     }
