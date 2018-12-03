70ffb54e82a42e269ed7288ce6d1f6036c445a2c
Alexandr Shefer <shefard55r@yandex.ru>
2018-12-03 12:50:59 +0600

    - Added test logging message for test endpoint

--- src/GeoPing.Api/Controllers/TestController.cs
+++ src/GeoPing.Api/Controllers/TestController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace GeoPing.Api.Controllers
 {
-    [Route("[Controller]")]
+    [Route("test")]
     [Authorize]
-    public class IdentityController : ControllerBase
+    public class TestController : ControllerBase
     {
+        private ILogger _logger;
+
+        public TestController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation($"TEST LOGGING BY USER {User.Claims.FirstOrDefault(x => x.Type == "sub").Value}");
+
             return new JsonResult(from c in User.Claims
                                   select new
                                   {
