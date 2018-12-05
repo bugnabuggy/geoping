a3f06a618fc4deba8ab32526ef30e3e357b73603
Alexandr Shefer <shefard55r@yandex.ru>
2018-12-05 18:19:35 +0600

    - CheckIn fields Distance and DeviceId are now nullable

--- src/GeoPing.Core/Models/Entities/CheckIn.cs
+++ src/GeoPing.Core/Models/Entities/CheckIn.cs
@@ -8,9 +8,9 @@ namespace GeoPing.Core.Models.Entities
         public Guid UserId { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public double Distance{ get; set; }
+        public double? Distance{ get; set; }
         public DateTime Date { get; set; }
-        public Guid DeviceId { get; set; }
+        public Guid? DeviceId { get; set; }
         public string Ip { get; set; }
         public string UserAgent { get; set; }
 
