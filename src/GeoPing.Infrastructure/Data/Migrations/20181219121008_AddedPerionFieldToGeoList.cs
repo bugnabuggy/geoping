d640d3af90c58b7425abfda48fc61bfdd9d38e97
Alexandr Shefer <shefard55r@yandex.ru>
2018-12-19 18:14:42 +0600

    - Added Period field to GeoList entity, - Added related migration, - Added Period field to GeolistDTO, - Added operations with Period for AddList() and EditList()

---
+++ src/GeoPing.Infrastructure/Data/Migrations/20181219121008_AddedPerionFieldToGeoList.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeoPing.Infrastructure.Data.Migrations
+{
+    public partial class AddedPerionFieldToGeoList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Period",
+                table: "GeoLists",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "GeoLists");
+        }
+    }
+}
